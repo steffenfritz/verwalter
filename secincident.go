257ccdebab3be638cddcd93e3dc0442e667bad87
Steffen Fritz <steffen@fritz.wtf>
2018-07-02 00:01:12 +0200

    working copy - 20180701

--- secincident.go
+++ secincident.go
@@ -1,1 +1,20 @@
 package main
+
+/* GENERAL NOTE:
+Introducing a sec incident to verwalter means introducing a ticketing systems. Which means a lot of work. Therefore postponded.
+*/
+
+// Secinc is the type that defines a security incident
+type Secinc struct {
+	reporterFirstName string
+	reporterEmail     string
+	reporterTelNo     string
+	reportedAsset     int
+	reportedService   int
+	reportedDate      string
+	shortInitDesc     string
+	longInitDesc      string
+	stillOpen         int
+	closedDate        string
+	forwardedTo       int
+}
