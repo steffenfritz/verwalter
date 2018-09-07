59b6decc5992a215128f92019e337f6b54232e4e
Steffen Fritz <steffen@fritz.wtf>
2018-09-07 22:27:29 +0200

    working copy - 20180907

--- reports.go
+++ reports.go
@@ -5,9 +5,18 @@ import (
 	"net/http"
 )
 
+// DefaultReport type is a struct that holds values for the default report
+type DefaultReport struct {
+	SecIncCountOpen    int
+	VulnCountOpen      int
+	AssetCountActive   int
+	PersonsCountActive int
+	OldestUpdate       int
+}
+
 // Reports handles requests to reports
 func Reports(w http.ResponseWriter, r *http.Request) {
-	var SecIncCountOpen int
+	var DefaultReport DefaultReport
 
 	tmpl, err := template.ParseFiles(Staticpath + "/templates/reports.tmpl")
 	e(err)
@@ -17,8 +26,24 @@ func Reports(w http.ResponseWriter, r *http.Request) {
 	defer rows.Close()
 
 	for rows.Next() {
-		err = rows.Scan(&SecIncCountOpen)
+		err = rows.Scan(&DefaultReport.SecIncCountOpen)
 	}
 
-	tmpl.Execute(w, SecIncCountOpen)
+	rows, err = db.Query("SELECT count(*) FROM assets where active = 'true'")
+	e(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&DefaultReport.AssetCountActive)
+	}
+
+	rows, err = db.Query("SELECT count(*) FROM persons")
+	e(err)
+	defer rows.Close()
+
+	for rows.Next() {
+		err = rows.Scan(&DefaultReport.PersonsCountActive)
+	}
+
+	tmpl.Execute(w, DefaultReport)
 }
